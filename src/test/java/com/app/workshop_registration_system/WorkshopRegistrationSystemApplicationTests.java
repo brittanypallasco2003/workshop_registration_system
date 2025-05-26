53522c1c9b67219e915b742712ddd36e7dda0294
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-26 11:02:27 -0500

    chore: initialize project

---
+++ src/test/java/com/app/workshop_registration_system/WorkshopRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.workshop_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
