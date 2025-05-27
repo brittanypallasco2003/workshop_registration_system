97c21207b6ccf8eb1f47053db07ddde55e5defd1
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-27 17:15:20 -0500

    feat: add method to find active workshops in WorkshopRepository

--- src/main/java/com/app/workshop_registration_system/Repositories/WorkshopRepository.java
+++ src/main/java/com/app/workshop_registration_system/Repositories/WorkshopRepository.java
@@ -1,9 +1,12 @@
 package com.app.workshop_registration_system.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.app.workshop_registration_system.Models.WorkshopModel;
 
 public interface WorkshopRepository extends CrudRepository<WorkshopModel, Long> {
 
+    List<WorkshopModel>findByActiveTrue();
 }
