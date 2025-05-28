409fc706ff43bbd771fbe2fd557d898df5161428
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-28 12:59:50 -0500

    feat: implement PUT and DELETE endpoints in WorkshopController

--- src/main/java/com/app/workshop_registration_system/Controllers/WorkshopController.java
+++ src/main/java/com/app/workshop_registration_system/Controllers/WorkshopController.java
@@ -3,6 +3,7 @@ package com.app.workshop_registration_system.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.workshop_registration_system.Advice.EntityNotFoundException;
 import com.app.workshop_registration_system.Models.DTO.WorkshopRequestDTO;
 import com.app.workshop_registration_system.Services.WorkshopServiceImpl;
 
@@ -11,11 +12,13 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -28,18 +31,43 @@ public class WorkshopController {
         this.workshopServiceImpl = workshopServiceImpl;
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAllWorkshops() {
-        return ResponseEntity.ok().body(workshopServiceImpl.getAllWorkshops());
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllWorkshopsAdmin() {
+        return ResponseEntity.ok().body(workshopServiceImpl.getAllWorkshopsAdmin());
     }
 
-    @PostMapping
-    public ResponseEntity<?> postMethodName(@RequestBody @Valid WorkshopRequestDTO workshopRequestDTO) {        
-        return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(workshopServiceImpl.createWorkshop(workshopRequestDTO));
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getSpecificWorkshop(@PathVariable Long id) {
+        return workshopServiceImpl.getSpecificWorkshop(id).map(workshop -> ResponseEntity.ok().body(workshop))
+                .orElseThrow(() -> new EntityNotFoundException("Taller " + id + " no encontrado"));
+    }
+
+    @GetMapping("/available")
+    public ResponseEntity<?> getAvailableWorkshops() {
+        return ResponseEntity.status(HttpStatus.OK).body(workshopServiceImpl.getAllActiveWorkshops());
     }
     
-    
+
+    @PostMapping("/create")
+    public ResponseEntity<?> postWorkshop(@RequestBody @Valid WorkshopRequestDTO workshopRequestDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(workshopServiceImpl.createWorkshop(workshopRequestDTO));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> putWorkshop(@PathVariable Long id,
+            @Valid @RequestBody WorkshopRequestDTO workshopRequestDTO) {
+        return workshopServiceImpl.updateWorkshop(workshopRequestDTO, id)
+                .map(workshopResponseDTO -> ResponseEntity.status(HttpStatus.OK).body(workshopRequestDTO))
+                .orElseThrow(()->new EntityNotFoundException("Taller "+id+" no encontrado"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteWorkshop(@PathVariable Long id) {
+        boolean deleted = workshopServiceImpl.deleteWorkshop(id);
+        return deleted ? ResponseEntity.noContent().build()
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Taller " + id + " no encontrado");
+    }
 
 }
