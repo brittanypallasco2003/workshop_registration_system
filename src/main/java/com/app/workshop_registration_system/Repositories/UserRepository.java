12e4612294c3f0ab7ae07e572b4f5502d8a87eca
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-30 11:29:33 -0500

    feat: add method to find user by email in WorkshopRepository

--- src/main/java/com/app/workshop_registration_system/Repositories/UserRepository.java
+++ src/main/java/com/app/workshop_registration_system/Repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.app.workshop_registration_system.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.app.workshop_registration_system.Models.UserModel;
 
 public interface UserRepository extends CrudRepository<UserModel, Long> {
+
+    Optional<UserModel> findByEmail(String email);
 
 }
