113cee739b73d7650ee990346beee0f3de65b299
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-30 11:36:32 -0500

    feat: secure WorkshopController endpoints with @PreAuthorize annotation

--- src/main/java/com/app/workshop_registration_system/Controllers/WorkshopController.java
+++ src/main/java/com/app/workshop_registration_system/Controllers/WorkshopController.java
@@ -10,16 +10,14 @@ import com.app.workshop_registration_system.Services.WorkshopServiceImpl;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/workshop")
@@ -31,23 +29,26 @@ public class WorkshopController {
         this.workshopServiceImpl = workshopServiceImpl;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
     public ResponseEntity<?> getAllWorkshopsAdmin() {
         return ResponseEntity.ok().body(workshopServiceImpl.getAllWorkshopsAdmin());
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PARTICIPANT')")
     @GetMapping("/{id}")
     public ResponseEntity<?> getSpecificWorkshop(@PathVariable Long id) {
         return workshopServiceImpl.getSpecificWorkshop(id).map(workshop -> ResponseEntity.ok().body(workshop))
                 .orElseThrow(() -> new EntityNotFoundException("Taller " + id + " no encontrado"));
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PARTICIPANT')")
     @GetMapping("/available")
     public ResponseEntity<?> getAvailableWorkshops() {
         return ResponseEntity.status(HttpStatus.OK).body(workshopServiceImpl.getAllActiveWorkshops());
     }
-    
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> postWorkshop(@RequestBody @Valid WorkshopRequestDTO workshopRequestDTO) {
         return ResponseEntity
@@ -55,14 +56,17 @@ public class WorkshopController {
                 .body(workshopServiceImpl.createWorkshop(workshopRequestDTO));
     }
 
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> putWorkshop(@PathVariable Long id,
             @Valid @RequestBody WorkshopRequestDTO workshopRequestDTO) {
         return workshopServiceImpl.updateWorkshop(workshopRequestDTO, id)
                 .map(workshopResponseDTO -> ResponseEntity.status(HttpStatus.OK).body(workshopRequestDTO))
-                .orElseThrow(()->new EntityNotFoundException("Taller "+id+" no encontrado"));
+                .orElseThrow(() -> new EntityNotFoundException("Taller " + id + " no encontrado"));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteWorkshop(@PathVariable Long id) {
         boolean deleted = workshopServiceImpl.deleteWorkshop(id);
