25275b901146f96244adddd56b741c51cff265c8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-30 11:28:32 -0500

    refactor: add error handling methods for 401, 403 and 500 in Auth REST controllers

---
+++ src/main/java/com/app/workshop_registration_system/Advice/UserExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.app.workshop_registration_system.Advice;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+@RestControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, Object>>handleAuthenticationError(BadCredentialsException exception){
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpStatus.UNAUTHORIZED);
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message",  "Error en la autenticación: email o password incorrectos");
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+    }
+
+    @ExceptionHandler(JWTCreationException.class)
+    public ResponseEntity<Map<String, Object>>handleJwtGenerateError(JWTCreationException exception){
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message",  exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<Map<String, Object>>handleAuthorizationDeniedException(AuthorizationDeniedException exception){
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpStatus.FORBIDDEN);
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message",  "Acceso denegado: No tienes permitido acceder a este recurso");
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(body);
+    }
+
+
+
+}
