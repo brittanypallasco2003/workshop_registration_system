86351fdc82fc93b1ee157298a2011a06a5af20f3
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-01 17:14:56 -0500

    feat: add business logic in EmailServiceImpl to send emails

---
+++ src/main/java/com/app/workshop_registration_system/Services/EmailServiceImpl.java
@@ -0,0 +1,84 @@
+package com.app.workshop_registration_system.Services;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.app.workshop_registration_system.Config.MailSenderProperties;
+import com.app.workshop_registration_system.Utils.EmailUtils;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final MailSenderProperties mailSenderProperties;
+
+    private final JavaMailSender javaMailSender;
+
+    private final EmailUtils emailUtils;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender, MailSenderProperties mailSenderProperties,
+            EmailUtils emailUtils) {
+        this.javaMailSender = javaMailSender;
+        this.mailSenderProperties = mailSenderProperties;
+        this.emailUtils = emailUtils;
+    }
+
+    @Override
+    public void sendEmail(String toUser, String subject, Map<String, String> variables) {
+
+        String html = emailUtils.loadHtmlTemplate("templates/email/registro_confirmado.html");
+
+        // Reemplazar variables en el HTML
+        for (Map.Entry<String, String> entry : variables.entrySet()) {
+            html = html.replace("[[" + entry.getKey() + "]]", entry.getValue());
+        }
+
+        MimeMessage message=javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper= new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+            helper.setFrom(mailSenderProperties.getEmailFrom());
+            helper.setTo(toUser);
+            helper.setSubject(subject);
+            helper.setText(html, true);
+            javaMailSender.send(message);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error al enviar correo de confirmación");
+        }
+   
+
+    }
+
+    @Override
+    public void sendEmailWithFile(String toUser, String subject, String message, File file) {
+
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true,
+                    StandardCharsets.UTF_8.name());
+
+            mimeMessageHelper.setFrom(mailSenderProperties.getEmailFrom());
+            mimeMessageHelper.setTo(toUser);
+            mimeMessageHelper.setText(message);
+            mimeMessage.setSubject(subject);
+
+            mimeMessageHelper.addAttachment(file.getName(), file);
+
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Error al enviar correo de confirmación");
+
+        }
+    }
+
+}
