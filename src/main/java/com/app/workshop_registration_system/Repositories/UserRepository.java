d8b1d43ab78e6d00bd8551df637de2140a87a725
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-02 19:40:14 -0500

    feat: add repository methods to find emails and phone numbers for uniqueness validation

--- src/main/java/com/app/workshop_registration_system/Repositories/UserRepository.java
+++ src/main/java/com/app/workshop_registration_system/Repositories/UserRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.app.workshop_registration_system.Models.UserModel;
 
+
 public interface UserRepository extends CrudRepository<UserModel, Long> {
 
     Optional<UserModel> findByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+    boolean existsByEmail(String email);
 
 }
