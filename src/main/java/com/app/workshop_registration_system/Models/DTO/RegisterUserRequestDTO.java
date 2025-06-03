2b477472e690b2b200dc065987f07cd0a1c77617
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-02 19:36:06 -0500

    feat: add @IsExistPhoneNumber annotation to validate phone uniqueness

--- src/main/java/com/app/workshop_registration_system/Models/DTO/RegisterUserRequestDTO.java
+++ src/main/java/com/app/workshop_registration_system/Models/DTO/RegisterUserRequestDTO.java
@@ -1,5 +1,8 @@
 package com.app.workshop_registration_system.Models.DTO;
 
+import com.app.workshop_registration_system.Validation.Annotation.IsExistEmail;
+import com.app.workshop_registration_system.Validation.Annotation.IsExistPhoneNumber;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,7 +11,7 @@ import jakarta.validation.constraints.Size;
 public record RegisterUserRequestDTO(
         @NotBlank @Size(min = 3, max = 20) String name,
         @NotBlank @Size(min = 3, max = 20) String lastname,
-        @NotBlank @Email String email,
+        @NotBlank @Email @IsExistEmail String email,
         @NotBlank @Size(min = 8) String password,
-        @NotBlank @Pattern(regexp = "^\\+593\\d{9}$", message = "Número de teléfono inválido") String phoneNumber) {
+        @NotBlank @Pattern(regexp = "^\\+593\\d{9}$", message = "Número de teléfono inválido") @IsExistPhoneNumber String phoneNumber) {
 }
