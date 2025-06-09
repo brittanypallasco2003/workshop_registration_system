4a81c0bd225d49dff84e3027cb5a83fbfd0bdf6c
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-09 15:16:50 -0500

    feat: implement GET endpoints in RegistrationController to retrieve all registrations by user ID

--- src/main/java/com/app/workshop_registration_system/Controllers/RegistrationController.java
+++ src/main/java/com/app/workshop_registration_system/Controllers/RegistrationController.java
@@ -2,12 +2,15 @@ package com.app.workshop_registration_system.Controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.workshop_registration_system.Models.DTO.Request.RegistrationRequestDTO;
+import com.app.workshop_registration_system.Models.DTO.Response.RegistrationResponseDTO;
 import com.app.workshop_registration_system.Services.RegistrationServiceImpl;
 
 import jakarta.validation.Valid;
 
+import java.net.URI;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/registration")
@@ -28,10 +33,21 @@ public class RegistrationController {
     }
 
     @PreAuthorize("hasRole('PARTICIPANT')")
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getAllRegistrationsByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok().body(registrationServiceImpl.getAllRegistrationByUser(userId));
+    }
+    
+    @PreAuthorize("hasRole('PARTICIPANT')")
     @PostMapping
     public ResponseEntity<?> postRegister(@RequestBody @Valid RegistrationRequestDTO requestDTO) {
-
-        return ResponseEntity.ok().body(registrationServiceImpl.createRegister(requestDTO));
+        RegistrationResponseDTO responseDTO = registrationServiceImpl.createRegister(requestDTO);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDTO.id())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDTO);
     }
 
     @PreAuthorize("hasRole('PARTICIPANT')")
