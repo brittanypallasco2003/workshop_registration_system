7bbba52263ada42339a09cc0c1eb1e59a46bccdc
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-09 13:41:53 -0500

    feat: define service method to retrieve all registrations by user ID

--- src/main/java/com/app/workshop_registration_system/Services/RegistrationService.java
+++ src/main/java/com/app/workshop_registration_system/Services/RegistrationService.java
@@ -1,11 +1,15 @@
 package com.app.workshop_registration_system.Services;
 
+import java.util.List;
+
 import com.app.workshop_registration_system.Models.DTO.Request.RegistrationRequestDTO;
 import com.app.workshop_registration_system.Models.DTO.Response.RegistrationResponseDTO;
+import com.app.workshop_registration_system.Models.DTO.Response.RegistrationResponseListDTO;
 
 public interface RegistrationService {
     RegistrationResponseDTO createRegister(RegistrationRequestDTO requestDTO);
     void cancelRegistration(Long id);
     RegistrationResponseDTO reactivateRegistration(Long id);
+    List<RegistrationResponseListDTO> getAllRegistrationByUser(Long id);
 
 }
