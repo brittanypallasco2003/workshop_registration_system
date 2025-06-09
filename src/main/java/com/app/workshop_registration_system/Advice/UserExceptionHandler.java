a41297c433b282d18b5c7a519a399323c5693372
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-09 15:44:00 -0500

    refactor: update RestControllerExceptionHandler to handle authorization errors

--- src/main/java/com/app/workshop_registration_system/Advice/UserExceptionHandler.java
+++ src/main/java/com/app/workshop_registration_system/Advice/UserExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,7 +21,7 @@ public class UserExceptionHandler {
     public ResponseEntity<Map<String, Object>>handleAuthenticationError(BadCredentialsException exception){
 
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("status", HttpStatus.UNAUTHORIZED);
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
         body.put("timestamp", LocalDateTime.now());
         body.put("message",  "Error en la autenticación: email o password incorrectos");
 
@@ -31,7 +32,7 @@ public class UserExceptionHandler {
     public ResponseEntity<Map<String, Object>>handleJwtGenerateError(JWTCreationException exception){
 
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         body.put("timestamp", LocalDateTime.now());
         body.put("message",  exception.getMessage());
 
@@ -43,9 +44,21 @@ public class UserExceptionHandler {
     public ResponseEntity<Map<String, Object>>handleAuthorizationDeniedException(AuthorizationDeniedException exception){
 
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("status", HttpStatus.FORBIDDEN);
+        body.put("status", HttpStatus.FORBIDDEN.value());
         body.put("timestamp", LocalDateTime.now());
         body.put("message",  "Acceso denegado: No tienes permitido acceder a este recurso");
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(body);
+    }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, Object>>handleAccessDeniedException(AccessDeniedException exception){
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", HttpStatus.FORBIDDEN.value());
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message",  exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(body);
     }
