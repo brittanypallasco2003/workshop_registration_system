83355b9577060d9ce212a52bada280501ff5e900
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-06-23 11:48:50 -0500

    chore: add configuration class for Swagger documentation

---
+++ src/main/java/com/app/workshop_registration_system/Config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.app.workshop_registration_system.Config;
+
+
+import org.springframework.http.HttpHeaders;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(info = @Info(title = "Workshop Registration System", description = "Sistema backend para la gestión de inscripciones a talleres", version = "1.0.0", contact = @Contact(name = "Brittany Espinel", url = "https://github.com/brittanypallasco2003", email = "nohemiespinel0@gmail.com")), servers = {
+        @Server(description = "PROD SERVER", url = "https://workshopregistrationsystem-production.up.railway.app/"),
+        @Server(description = "DEV SERVER", url = "http://localhost:8080")},security = @SecurityRequirement(name = "Security Token"))
+@SecurityScheme(name = "Security Token", description = "Access Token for my API", type = SecuritySchemeType.HTTP, paramName = HttpHeaders.AUTHORIZATION, in = SecuritySchemeIn.HEADER, scheme = "bearer", bearerFormat = "JWT")
+public class SwaggerConfig {
+
+}
