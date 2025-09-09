52ed17144d55db228401e853f78d8ffe69bb7ad6
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-09-08 21:26:29 -0500

    fix: actualizar URL del servidor de producción en la configuración de Swagger

--- src/main/java/com/app/workshop_registration_system/Config/SwaggerConfig.java
+++ src/main/java/com/app/workshop_registration_system/Config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(info = @Info(title = "Workshop Registration System", description = "Sistema backend para la gestión de inscripciones a talleres", version = "1.0.0", contact = @Contact(name = "Brittany Espinel", url = "https://brittany-portfolio.vercel.app/", email = "nohemiespinel0@gmail.com")), servers = {
-        @Server(description = "PROD SERVER", url = ""),
+        @Server(description = "PROD SERVER", url = "https://workshop-registration-system.onrender.com/"),
         @Server(description = "DEV SERVER", url = "http://localhost:8080")},security = @SecurityRequirement(name = "Security Token"))
 @SecurityScheme(name = "Security Token", description = "Access Token for my API", type = SecuritySchemeType.HTTP, paramName = HttpHeaders.AUTHORIZATION, in = SecuritySchemeIn.HEADER, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
